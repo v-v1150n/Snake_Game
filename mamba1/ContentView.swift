59f80b8542cf933e970688e90f6e29163b4d1e7c
Your Name <your.email@example.com>
2025-02-25 12:15:03 +0800

    Initial Commit

---
+++ mamba1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mamba1
+//
+//  Created by v_v1150n on 2025/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mamba1/mamba1App.swift
@@ -0,0 +1,17 @@
+//
+//  mamba1App.swift
+//  mamba1
+//
+//  Created by v_v1150n on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct mamba1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
